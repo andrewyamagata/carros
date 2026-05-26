cf207e2514050eafec573ef8a7fc721df43a2c17
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-05-26 15:39:35 -0300

    images

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# Importar os settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Depois do urlpatterns, adicionar o + static (tudo isso para o armazenamento de imagens)
